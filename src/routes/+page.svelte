71c2658cdaebec6d2650f38b32b04d51a9993f04
jeeradate <jeeradate@gmail.com>
2024-09-12 17:30:16 +0700

    add collabsibe section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,47 @@
 <script>
-    import src from '$lib/assests/autohotkey.png';
-    import Hello from '$lib/components/Hello.svelte';
-
+	import src from '$lib/assests/autohotkey.png';
+	import Hello from '$lib/components/Hello.svelte';
 </script>
 
+<h1>Hello</h1>
+<h3>
+	This web site is run on free host sponsor from <a href="http://vercel.com" target="_blank"
+		>Vercel</a
+	>
+</h3>
+<details>
+    <summary>Hosting web for free on Vercel.com</summary>
+    <p style="font-size: 1.5em ;">
+        เมื่อคุณเขียน App หรือ เว็บด้วย Svelte หรือ Sveltekit แล้วก็ย่อมอยากดู App หรือเว็บใน
+        Hosting จริงๆ ว่าจะเป็นอย่างไร โดยเว็บนี้ก็ได้ Free host จาก Vercel ซึ่งในการทำก็มีหลายวิธี
+        แต่วิธีที่ผมคิดว่าดีและสะดวก คือการใช้ GitHub ช่วย เพราะ GitHub จะช่วยรักษา Version ต่างๆ
+        ของ App หรือเว็บของคุณได้ด้วย <br />
+        วิธีการ
+    </p>
 
-<h1>Welcome to SvelteKit</h1>
-<h1 class="myH1"><Hello /></h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<img src="{src}" alt=""  style="width: 300px;">
+    <div style="font-size: 1.5em;">
+        <ul>
+            <li>ลงทะเบียน <a href="https://github.com/">GitHub</a></li>
+            <li>ลงทะเบียน <a href="https://vercel.com/">Vercel</a></li>
+            <li>เปิด Project ใน VSCode จากนั้น upload Project ขึ้น GitHub</li>
+            <li>
+                จากนั้นเข้าเว็ป Vercel โดยการ Login ด้วย GitHub และเลือก Project ใน GitHub เพื่อ Deploy
+                เท่านั้นก็จบครับ
+            </li>
+            <li>
+                ดูคลิปนี้เพื่อดูรายละเอียดนะครับ <a
+                    href="https://youtu.be/nyPCEH2xuhM?si=qs6AoRuLXy_3SeBL"
+                    target="_blank"
+                    >สอน Deploy Next.js เว็บไซต์ขึ้น Web Hosting ฟรี ด้วย GitHub + Vercel - YouTube</a
+                >
+            </li>
+        </ul>
+    </div>
+</details>
 
-<style>
-    .myH1 {
-        color: blueviolet;
-    } 
-</style>
+<details>
+    <summary>Section 2</summary>
+    <p>This is the content of section 2.</p>
+</details>
+
+
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+
+
+<div>
+
+    <details>
+        <summary>Section 1</summary>
+        <p>This is the content of section 1.</p>
+      </details>
+    
+      <details>
+        <summary>Section 2</summary>
+        <p>This is the content of section 2.</p>
+      </details>
+</div>
